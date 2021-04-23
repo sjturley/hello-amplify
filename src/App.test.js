69adecb69e8c0228045e43f36acf75ec224b692a
Steve Turley <steve@turleylabs.com>
2021-04-23 16:20:55 -0500

    Trying build again

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-  // trigger build
-  expect(false).toEqual(true);
 });
